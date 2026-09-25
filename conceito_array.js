e4da6a6da8ff4b4cfd32707c867eb12d519ae20f
giocassini <giovannacarvalho510@gmail.com>
2026-09-25 13:39:44 -0300

    Add array examples for fruits and animals
    Introduced an array example with fruits and animals, demonstrating array operations.

---
+++ conceito_array.js
@@ -0,0 +1,20 @@
+// Array
+// Um array é uma lista que pode armazenar vários valores.
+
+let frutas = ["Maçã", "Banana", "Uva", "Melancia", "Manga"];
+// let fruta = ["0", "1", "2", "3", "4"]
+
+console.log(frutas[3]);
+
+// Podemos adicionar itens!, sem alterar o array diretamente
+frutas.push("Laranja"); //Adiciona ao final
+console.log(frutas[5]);
+frutas.pop(frutas[1]);
+console.log(frutas);
+
+// Crie um array chamado `animais` e adcione três animais.
+// Exiba o primeiro e o último no console.
+
+let animais = ["Ornitorrinco", "Zebra", "Canguru"];
+console.log(animais[0]);
+console.log(animais[2]);

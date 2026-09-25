e12d8412b75bf13f4aafa559158af74d695df4f4
giocassini <giovannacarvalho510@gmail.com>
2026-09-25 13:38:31 -0300

    Create conceitos_objeto.js

---
+++ conceitos_objeto.js
@@ -0,0 +1,27 @@
+
+// Object (Objetos)
+// Um ovjeto agrupa várias informações dentro de uma mesma variável, usando chave e valor
+
+let pessoa = {
+    nome: "Ana",
+    idade: 18,
+    cidade: "São Paulo" // O último elemento não precisa de vírgula
+};
+
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`A ${pessoa.nome} tem ${pessoa.idade} anos de idade`);
+console.log(`-----------------------------------------------`)
+
+// Crie um objeto chamado `carro` com as propriedades `marca`, `modelo` e `ano`.
+// Exiba cada propriedade no console.
+
+let carro = {
+    marca: "Chevrolet",
+    modelo: "Camaro ZL1",
+    ano: 2012
+};
+
+console.log(`Marca: ${carro.marca}`);
+console.log(`Modelo: ${carro.modelo}`);
+console.log(`Ano: ${carro.ano}`);
+console.log(`-----------------------------------------------`);
